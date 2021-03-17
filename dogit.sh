9f619a857751e1179f856658fac3a58dda7bf069
AmosNimos <amounimous@protonmail.com>
2021-03-16 20:33:41 -0400

    Update

--- dogit.sh
+++ dogit.sh
@@ -1,5 +1,5 @@
 git init
 git add *
 git commit -m "Update"
-git remote add origin https://github.com/AmosNimos/text-rpg.git
+#git remote add origin https://github.com/AmosNimos/text-rpg.git
 git push -u origin master
